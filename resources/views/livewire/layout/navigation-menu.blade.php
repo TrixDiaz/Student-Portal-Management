72880ccbcfa171aef6726c2ccbfdd83e8346ce0e
XirTech <john.darlucio022@gmail.com>
2024-12-06 11:29:18 +0800

    student nav

--- resources/views/livewire/layout/navigation-menu.blade.php
+++ resources/views/livewire/layout/navigation-menu.blade.php
@@ -25,6 +25,14 @@
                     </form>
                 </div>
 
+                @if (auth()->user()->hasRole('student'))
+                <div class="hidden sm:flex items-center justify-center ml-4">
+                    <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                        {{ __('Home') }}
+                    </x-nav-link>
+                </div>
+                @endif
+
                 <div class="hidden sm:flex items-center justify-center ml-4"
                     x-data="{ time: new Date().toLocaleTimeString('en-US', { timeZone: 'Asia/Manila', hour12: false }) }"
                     x-init="setInterval(() => time = new Date().toLocaleTimeString('en-US', { timeZone: 'Asia/Manila', hour12: false }), 1000)">
@@ -32,6 +40,8 @@
                         {{ now()->format('l, F j, Y') }} <span x-text="time"></span>
                     </p>
                 </div>
+
+
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ms-6">
@@ -266,9 +276,11 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <!-- <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link> -->
+            @if (auth()->user()->hasRole('student'))
+            <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                {{ __('Home') }}
+            </x-responsive-nav-link>
+            @endif
         </div>
 
         <!-- Responsive Settings Options -->
