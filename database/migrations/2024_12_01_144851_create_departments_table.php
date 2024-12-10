13d766876305acfd68c4d71ee6f6c0c83d4d82bd
XirTech <john.darlucio022@gmail.com>
2024-12-10 14:02:39 +0800

    deleted at in department

--- database/migrations/2024_12_01_144851_create_departments_table.php
+++ database/migrations/2024_12_01_144851_create_departments_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('course');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
