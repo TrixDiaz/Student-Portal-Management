deaaa1926665f80989e1473293b029c63d7b3d31
XirTech <john.darlucio022@gmail.com>
2024-12-11 21:56:07 +0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,7 +26,7 @@ class DatabaseSeeder extends Seeder
             PhaseSeeder::class,
             QuestionSeeder::class,
             SectionSeeder::class,
-            QuestionResponseSeeder::class,
+            // QuestionResponseSeeder::class,
         ]);
     }
 }
