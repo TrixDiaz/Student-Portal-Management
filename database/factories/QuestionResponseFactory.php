873916db512691617f9d877df6637599be3815a9
XirTech <john.darlucio022@gmail.com>
2024-12-11 21:44:05 +0800

    Factories

--- database/factories/QuestionResponseFactory.php
+++ database/factories/QuestionResponseFactory.php
@@ -2,6 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\EvaluationResponse;
+use App\Models\Question;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,10 +20,10 @@ class QuestionResponseFactory extends Factory
     public function definition(): array
     {
         return [
-            'evaluation_response_id' => \App\Models\EvaluationResponse::factory(),
-            'question_id' => \App\Models\Question::factory(),
-            'student_id' => \App\Models\User::factory(),
-            'rating' => $this->faker->numberBetween(1, 5), // Assuming rating is 1-5
+            'evaluation_response_id' => EvaluationResponse::factory(),
+            'question_id' => Question::factory(),
+            'student_id' => User::factory(),
+            'rating' => $this->faker->numberBetween(1, 5),
         ];
     }
 }
